01a6b53dd3cf2dd14c03c0fc4c619c6db73c2a47
Faizal <faizalamin000@gmail.com>
2023-04-25 00:36:54 +0530

    assingment2 is done

--- src/routes/route.js
+++ src/routes/route.js
@@ -126,4 +126,80 @@ router.get('/flims/:flimId',function(req,res){
       
 })
 
+   // -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+ // Your route code will look like this
+ router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+    let sum = arr.reduce((a,b)=>a+b)
+    let arrayPerfectSum = arr[arr.length-1]*(arr[arr.length-1]+1)/2 
+    let missingNumber = arrayPerfectSum - sum
+
+    ///LOGIC WILL GO HERE 
+    res.send(  { data: missingNumber  }  );
+});
+
+//Q2.
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+// Your route code will look like this
+router.get("/sol2", function (req, res) {
+        //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+        let arr= [33, 34, 35, 37, 38]
+        let sum = arr.reduce((a,b)=>a+b)
+        let arrayPerfectSum = (arr.length+1)*(arr[0]+arr[arr.length-1])/2
+        let missingNumber = arrayPerfectSum - sum
+
+        ///LOGIC WILL GO HERE 
+
+
+        res.send(  { data: missingNumber  }  );
+});
+
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+   router.post('/players', function (req, res) {
+
+
+    //LOGIC WILL COME HERE
+    let newPlayer =req.body
+    players.push(newPlayer)
+    if(players.find(x=>x.name === newPlayer.name)){
+        return res.send(`${newPlayer.name}  is already exist in players`)
+    }
+    return res.send(  { data: players , status: true }  )
+})
+
+
+
+
+
+
 module.exports = router;
